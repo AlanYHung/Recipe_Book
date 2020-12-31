56d08ba02363e113b34756b72aecff69aa78dd88
AlanYHung <AlanYHung@hotmail.com>
2020-12-30 18:46:53 -0800

    Final Touches

--- public/js/login.js
+++ public/js/login.js
@@ -1,18 +1,33 @@
 'use strict';
 
+/* ======================================================================= */
+/* ================== Fridge Animation Event Listener ==================== */
+/* ======================================================================= */
+
+
 let $leftFridgeDoor = $('#left_fridge_door');
 let $rightFridgeDoor = $('#right_fridge_door');
 let $leftFridgeDoorOpen = $('#left_fridge_door_open');
 let $rightFridgeDoorOpen = $('#right_fridge_door_open');
+let $clickText = $('#click_me_modal_text');
+let $clickModal = $('#click_me_modal');
+let $overlayModal = $('#modal_overlay');
+
 
 $leftFridgeDoor.on('click', openFridgeDoor);
 $rightFridgeDoor.on('click', openFridgeDoor);
 $leftFridgeDoorOpen.on('click', closeFridgeDoor);
 $rightFridgeDoorOpen.on('click', closeFridgeDoor);
+$clickText.on('click', openFridgeDoor);
+$clickModal.on('click', openFridgeDoor);
+$overlayModal.on('click', openFridgeDoor);
 
 function openFridgeDoor(){
   $leftFridgeDoor.hide();
   $rightFridgeDoor.hide();
+  $clickModal.hide();
+  $clickText.hide();
+  $overlayModal.hide();
   $('#left_fridge_door_open').show();
   $('#right_fridge_door_open').show();
 }
@@ -22,4 +37,7 @@ function closeFridgeDoor(){
   $rightFridgeDoor.show();
   $('#left_fridge_door_open').hide();
   $('#right_fridge_door_open').hide();
+  $clickModal.show();
+  $clickText.show();
+  $overlayModal.show();
 }
